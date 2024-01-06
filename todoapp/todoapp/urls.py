429c494f14fdb5a090b5941f7ccc15498f1034b6
Sergio Navarrete <sergionvte11@gmail.com>
2024-01-06 01:34:37 -0600

    agregados distintos templates y configuraciones extras

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
-from webapp.views import index
+from webapp.views import index, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__reload__/', include('django_browser_reload.urls')),
     path('', index, name = 'index'),
+    path('register/', register, name = 'register'),
+    path('login/', auth_views.LoginView.as_view(), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
 ]
--- todoapp/webapp/views.py
+++ todoapp/webapp/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    logged = False
+
+    if logged:
+        values = {
+            'logged': logged,
+            'name': 'Sergio',
+            'todos': [
+                'Task 1',
+                'Task 2',
+                'Task 3',
+                'Task 4',
+                'Task 5',
+                'Task 6',
+                'Task 7',
+                'Task 8',
+                'Task 9 ',
+                'Task 10',
+            ]
+        }
+    else:
+        values = {'logged': logged}
+
+    return render(request, 'index.html', values)
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'auth/register.html', {'form': form})
